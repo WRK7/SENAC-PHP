6d0e977cf7f5305fc6183d61dc75794f94088cff
Wesley Cruz WRK7 <wesleyc09gomes@gmail.com>
2025-07-24 17:26:38 -0400

    Add files via upload

---
+++ PHP/action.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Olá mundo do PHP</h1>
+    <?php 
+    $nome=$_POST ["nome"];
+    echo "O nome digitado foi $nome<br>";?>
+</body>
+</html>
