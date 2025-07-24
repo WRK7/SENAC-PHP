de1cd5a9bf870ddd31cc88f5782c4e5309ba8b8d
Wesley Cruz WRK7 <wesleyc09gomes@gmail.com>
2025-07-24 17:23:11 -0400

    Add files via upload

---
+++ PHP/script.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="action.php">
+        <fildset id="pessoal">
+            <p>Nome <input type="text" name="nome" id="nome" size="20" maxlength="20" placeholder="Ex: João"> </p>
+        </fieldset>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
